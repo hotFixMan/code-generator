da508e565214f34ae78b31365e275bf75bbf06eb
ljc391321729 <391321729@qq.com>
2019-01-08 19:49:26 +0800

    fix:Application implements CommandLineRunner

--- src/main/java/org/own/generator/codegen/Application.java
+++ src/main/java/org/own/generator/codegen/Application.java
@@ -1,14 +1,30 @@
 package org.own.generator.codegen;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+@Log4j2
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer implements CommandLineRunner {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return super.configure(builder);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		log.info(">>>>>>>>> Code generator service launched");
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		log.info(">>>>>>>>> Start generating code");
+		//TODO 生成调用
+	}
 }
 
